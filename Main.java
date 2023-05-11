883a047fafc09d0acc7028da2ebd4cf7dd3b7d0a
JackyFC534 <2130173@upv.edu.mx>
2023-05-11 14:56:33 -0600

    Modificaciones correctas v1
    Cambio en Main y SumaDosNumeros . java  v1

--- Main.java
+++ Main.java
@@ -5,9 +5,7 @@ class Main {
    	int a = Integer.paserInt(args[0]);
     	int b = Integer.paserInt(args[1]); 
     
-	SumaDosNumeros obj = new SumaDosNumeros(); // new    nombre de la clase ()
-	//SumaDosNumeros obj2 = new SumaDosNumeros();
-	//SumaDosNumeros obj3 = new SumaDosNumeros();
+	SumaDosNumeros obj = new SumaDosNumeros();
 	
 	System.out.println(obj.suma(a,b));
     }
